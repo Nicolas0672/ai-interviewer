104e12e95181938d8b2577855db09305b35e20cf
Nicolas0672 <140045078+Nicolas0672@users.noreply.github.com>
2025-02-26 02:40:17 -0800

    Fixed Landing Page. Officially done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,29 @@
-import Image from "next/image";
+"use client"
+import { useUser } from '@clerk/nextjs';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default function Home() {
-  return (
-   <h1>hi</h1>
-  );
+function HomePage() {
+  const { isSignedIn, isLoading } = useUser(); // Check if the user is signed in
+  const router = useRouter();
+  const [redirecting, setRedirecting] = useState(false);
+
+  // Handle redirection logic
+  useEffect(() => {
+    if (isSignedIn) {
+      setRedirecting(true);
+      router.push('/dashboard'); // Redirect to dashboard if signed in
+    }
+  }, [isSignedIn, router]);
+
+  // Redirect to the sign-in page if not signed in
+  useEffect(() => {
+    if (!isSignedIn && !isLoading) {
+      router.push('/sign-in');
+    }
+  }, [isSignedIn, isLoading, router]);
+
+  
 }
+
+export default HomePage;
